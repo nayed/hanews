726bcf841de466b9f2d396042092a96397e71937
Nayed Saïd Ali <nayed.saidali@gmail.com>
2017-02-13 06:30:49 +0100

    button dismiss

--- src/App.js
+++ src/App.js
@@ -18,13 +18,37 @@ const list = [
     points: 5,
     objectID: 1,
   },
+  {
+    title: 'HaNews',
+    url: 'https://nayed.github.io/hanews',
+    author: 'Nayed Saïd Ali',
+    num_comments: 6,
+    points: 2,
+    objectID: 2,
+  },
 ]
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      list
+    }
+
+    this.onDismiss = this.onDismiss.bind(this)
+  }
+
+  onDismiss(id) {
+    const isNotId = item => item.objectID !== id
+    const updatedList = this.state.list.filter(isNotId)
+    this.setState({ list: updatedList })
+  }
+
   render() {
     return (
       <div className="App">
-        { list.map(item =>
+        { this.state.list.map(item =>
           <div key={item.objectID}>
             <span>
               <a href={item.url}>{item.title}</a>
@@ -32,6 +56,14 @@ class App extends Component {
             <span>{item.author}</span>
             <span>{item.num_comments}</span>
             <span>{item.points}</span>
+            <span>
+              <button
+                onClick={() => this.onDismiss(item.objectID)}
+                type="button"
+              >
+                Dismiss
+              </button>
+            </span>
           </div>
         )}
       </div>
